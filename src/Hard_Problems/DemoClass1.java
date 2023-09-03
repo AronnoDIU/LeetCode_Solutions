9df98ab4892547256c8ef487c4e83d738fc15036
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-09-03 07:32:20 +0600

    LeetCode Update

---
+++ src/Hard_Problems/DemoClass1.java
@@ -0,0 +1,7 @@
+package Hard_Problems;
+
+public class DemoClass1 {
+    public static void main(String[] args) {
+        System.out.println("Hard Problems only");
+    }
+}
---
+++ src/Medium_Problems/DemoClass2.java
@@ -0,0 +1,7 @@
+package Medium_Problems;
+
+public class DemoClass2 {
+    public static void main(String[] args) {
+        System.out.println("medium Problems only");
+    }
+}
